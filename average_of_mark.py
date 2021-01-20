cf89c6b37afa1522e42e68afb7ff17ed109706e9
EC2 Default User <ec2-user@ip-172-31-84-131.ec2.internal>
2021-01-20 01:14:24 +0000

    average of mark

---
+++ average_of_mark.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# Created by: Donggeun Lim
+# Created on: Jan 2019
+# This program calculates average of mark
+
+def main():
+    # this function uses a list to calculate the average of marks
+
+    marks = []
+    each_mark = None
+    total = 0
+
+    # input
+    print("Please enter 1 mark at a time. Enter -1 to end.")
+
+    each_mark = input("What is your mark? (as %): ")
+    marks.append(each_mark)
+    while each_mark != "-1":
+        each_mark = input("What is your mark? (as %): ")
+        marks.append(each_mark)
+
+    marks.pop()  # remove the "-1" that was added
+    print("")
+
+    for counter in range(0, len(marks)):
+        total += int(marks.pop())
+
+    average = total / (counter + 1)
+    print("The average is: {0}".format(average))
+
+
+if __name__ == "__main__":
+    main()
